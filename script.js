ff5fece85a1c40271f309ec24507de492238108b
bonbongarzon <97497863+bonbongarzon@users.noreply.github.com>
2022-11-25 14:27:03 +0800

    226

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-
-
-
 function toggleRegistration() {
+    hideCvSUCourses()
+
+
     var reqRegGuide = document.getElementById("reg-btn")
-    var regContent = document.getElementById("content2")
+    var regContent = document.getElementById("guides")
     var endFileRequestBtn = document.getElementById("end-cta-btn")
     regContent.style.display = "block";
-    endFileRequestBtn .style.display = "block";
+    endFileRequestBtn.style.display = "block";
     reqRegGuide.style.color = "#29711D";
-    
+
 }
 
-function showRegistratMainContents(){
-    toggleRegistration();
+function closeRegistration() {
+    var reqRegGuide = document.getElementById("reg-btn")
+    var regContent = document.getElementById("guides")
+    var endFileRequestBtn = document.getElementById("end-cta-btn")
+
+    regContent.style.display = "none";
+    endFileRequestBtn.style.display = "none";
+    reqRegGuide.style.color = "#1D1D1D";
+
+}
+
+function showCvSUCourses() {
+    closeRegistration()
+
+    var btn = document.getElementById("courses-btn")
+    var content = document.getElementById("reg_courses")
+
+    content.style.display = "block"
+}
+
+function hideCvSUCourses() {
+
+    var btn = document.getElementById("courses-btn")
+    var content = document.getElementById("reg_courses")
+
+    content.style.display = "none"
+
 }
